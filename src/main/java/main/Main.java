103054cd514c7d6276c14c3d5b708f9b0687a62d
Barity_ <mir3292@gmail.com>
2022-11-20 21:43:30 +0900

    활성화 메시지 변경

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,11 +8,11 @@ public final class Main extends JavaPlugin {
     private static final ConsoleCommandSender LOGGER = Bukkit.getConsoleSender();
     @Override
     public void onEnable() {
-        LOGGER.sendMessage("§4[§2Zombie§4] §a플러그인이 활성화되었습니다.");
+        LOGGER.sendMessage("§4[§2ZombieSurvival§4] §a플러그인이 활성화되었습니다.");
     }
 
     @Override
     public void onDisable() {
-        LOGGER.sendMessage("§4[§2Zombie§4] §c플러그인이 비활성화되었습니다.");
+        LOGGER.sendMessage("§4[§2ZombieSurvival§4] §c플러그인이 비활성화되었습니다.");
     }
 }
