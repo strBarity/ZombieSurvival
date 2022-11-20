e8f8ae3267fae4a2149f81014690446d69f8444d
Barity_ <mir3292@gmail.com>
2022-11-20 21:23:11 +0900

    프로젝트 생성

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+    private static final ConsoleCommandSender LOGGER = Bukkit.getConsoleSender();
+    @Override
+    public void onEnable() {
+        LOGGER.sendMessage("§4[§2Zombie§4] §a플러그인이 활성화되었습니다.");
+    }
+
+    @Override
+    public void onDisable() {
+        LOGGER.sendMessage("§4[§2Zombie§4] §c플러그인이 비활성화되었습니다.");
+    }
+}
